9fc1235c7866587cb3ca9d70894d1a7b1bc9a48a
= <kaven.feng@autodesk.com>
2014-01-26 17:40:34 +0800

    Pagecontrol is working now.

--- KidDrawing/DetailsViewController.m
+++ KidDrawing/DetailsViewController.m
@@ -12,6 +12,7 @@
 @interface DetailsViewController ()
 {
     UIPageControl* pageControl;
+    UIScrollView* sv;
 }
 @end
 
@@ -31,19 +32,21 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 
+    
     int num = 4;
     int imageW = 300;
     int imageH = 300;
     
+    self.navigationItem.title = self.name;
     // SrollView
-    UIScrollView* sv = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    sv = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     
     sv.contentSize = CGSizeMake(self.view.frame.size.width * num,self.view.frame.size.height);
     [self.view addSubview:sv];
     
     sv.pagingEnabled = YES;
-    sv.showsHorizontalScrollIndicator = YES;
-    sv.showsVerticalScrollIndicator  = YES;
+    sv.showsHorizontalScrollIndicator = NO;
+    sv.showsVerticalScrollIndicator  = NO;
     [sv setDelegate:self];
     
     for (int i = 0; i < num; i++) {
@@ -63,7 +66,9 @@
     }
     
     // Page Control
-    pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 600, self.view.frame.size.width, 20)];
+    pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 20, self.view.frame.size.width, 20)];
+    pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
+    pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
     [self.view addSubview:pageControl];
     
     pageControl.numberOfPages = num;
@@ -83,5 +88,33 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation
+                                   duration:duration];
+
+//    if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+//        int w = self.view.bounds.size.width;
+//        int h = self.view.bounds.size.height;
+//        self.view.frame = CGRectMake(0, 0, 568, 300);
+//        
+//        sv.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//        sv.contentSize = CGSizeMake(self.view.frame.size.width * 4,self.view.frame.size.height);
+//        
+//    }else if(toInterfaceOrientation == UIInterfaceOrientationLandscapeRight){
+//        int w = self.view.bounds.size.width;
+//        int h = self.view.bounds.size.height;
+//        self.view.frame = CGRectMake(0, 0, 568, 300);
+//        sv.contentSize = CGSizeMake(self.view.frame.size.width * 4,self.view.frame.size.height);
+//
+//        
+//    }else if (toInterfaceOrientation == UIInterfaceOrientationPortrait){
+//        self.view.frame = CGRectMake(0, 0, 300, 568);
+//        sv.contentSize = CGSizeMake(self.view.frame.size.width * 4,self.view.frame.size.height);
+//    }
+    
+}
+
 
 @end
