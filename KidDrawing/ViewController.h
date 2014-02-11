9f7f1a2caab3b3ed2195dac65d73a78bd30473c2
= <kaven.feng@autodesk.com>
2014-02-11 21:25:32 +0800

    Integrated iAd.

--- KidDrawing/ViewController.h
+++ KidDrawing/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
-@interface ViewController : UICollectionViewController
+@interface ViewController : UICollectionViewController <ADBannerViewDelegate>
 
 @end
--- KidDrawing/ViewController.m
+++ KidDrawing/ViewController.m
@@ -14,6 +14,8 @@
 @interface ViewController ()
 {
     NSMutableArray* fileThumbnails;
+    ADBannerView *_bannerView;
+
 }
 @end
 
@@ -28,9 +30,15 @@
     NSString* path = [[NSBundle mainBundle] pathForResource:@"FileLists" ofType:@"plist"];
     NSDictionary* dic = [[NSDictionary alloc] initWithContentsOfFile:path];
     fileThumbnails = [dic objectForKey:@"Thumbnails"];
-    
-//    UICollectionViewFlowLayout *flow = (UICollectionViewFlowLayout*)self.collectionViewLayout;
-//    flow.sectionInset = UIEdgeInsetsMake(5, 5, 0, 5);
+
+    // Init banner view
+    if ([ADBannerView instancesRespondToSelector:@selector(initWithAdType:)]) {
+        _bannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
+    } else {
+        _bannerView = [[ADBannerView alloc] init];
+    }
+    _bannerView.delegate = self;
+    [self.view addSubview:_bannerView];
     
 }
 
@@ -79,6 +87,67 @@
         DetailsViewController* detailed = (DetailsViewController*)segue.destinationViewController;
         detailed.name = fileThumbnails[path0.row];
     }
+}
+
+- (void)layoutAnimated:(BOOL)animated
+{
+    CGRect contentFrame = self.view.bounds;
+    
+    // all we need to do is ask the banner for a size that fits into the layout area we are using
+    CGSize sizeForBanner = [_bannerView sizeThatFits:contentFrame.size];
+    
+    // compute the ad banner frame
+    CGRect bannerFrame = _bannerView.frame;
+    if (_bannerView.bannerLoaded) {
+        
+        // bring the ad into view
+        contentFrame.size.height -= sizeForBanner.height;   // shrink down content frame to fit the banner below it
+        bannerFrame.origin.y = contentFrame.size.height;
+        bannerFrame.size.height = sizeForBanner.height;
+        bannerFrame.size.width = sizeForBanner.width;
+        
+//        // if the ad is available and loaded, shrink down the content frame to fit the banner below it,
+//        // we do this by modifying the vertical bottom constraint constant to equal the banner's height
+//        //
+//        NSLayoutConstraint *verticalBottomConstraint = self.bottomConstraint;
+//        verticalBottomConstraint.constant = sizeForBanner.height;
+//        [self.view layoutSubviews];
+        
+    } else {
+        // hide the banner off screen further off the bottom
+        bannerFrame.origin.y = contentFrame.size.height;
+    }
+    
+    [UIView animateWithDuration:animated ? 0.25 : 0.0 animations:^{
+//        _contentView.frame = contentFrame;
+//        [_contentView layoutIfNeeded];
+        _bannerView.frame = bannerFrame;
+    }];
+}
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    [self layoutAnimated:YES];
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"didFailToReceiveAdWithError %@", error);
+    [self layoutAnimated:YES];
+}
+
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave
+{
+    return YES;
+}
+
+- (void)bannerViewActionDidFinish:(ADBannerView *)banner
+{
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [self layoutAnimated:[UIView areAnimationsEnabled]];
 }
 
 @end
